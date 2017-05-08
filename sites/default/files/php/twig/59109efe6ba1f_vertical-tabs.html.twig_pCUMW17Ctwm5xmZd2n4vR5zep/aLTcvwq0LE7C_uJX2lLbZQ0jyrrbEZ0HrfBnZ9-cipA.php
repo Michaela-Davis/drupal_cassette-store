9946be96d10c85da44fdf81ba8157331e33b7fcd
Michaela Davis and Sarah Leahy <michaela+sarah@>
2017-05-08 15:25:03 -0700

    modify files to turn debugging on

--- sites/default/files/php/twig/59109efe6ba1f_vertical-tabs.html.twig_pCUMW17Ctwm5xmZd2n4vR5zep/aLTcvwq0LE7C_uJX2lLbZQ0jyrrbEZ0HrfBnZ9-cipA.php
+++ sites/default/files/php/twig/59109efe6ba1f_vertical-tabs.html.twig_pCUMW17Ctwm5xmZd2n4vR5zep/aLTcvwq0LE7C_uJX2lLbZQ0jyrrbEZ0HrfBnZ9-cipA.php
@@ -1,7 +1,7 @@
 <?php
 
-/* {# inline_template_start #}use-ajax */
-class __TwigTemplate_fbf1a3b5b9786d34f7824e96ac02da4c80faa9116e3833cabc7e1a94e0911c1c extends Twig_Template
+/* core/themes/classy/templates/navigation/vertical-tabs.html.twig */
+class __TwigTemplate_fc72180ae77beb59156a65e3701043ca922a1a3f1a316a855c2e9ea203e18aa5 extends Twig_Template
 {
     public function __construct(Twig_Environment $env)
     {
@@ -39,22 +39,45 @@ class __TwigTemplate_fbf1a3b5b9786d34f7824e96ac02da4c80faa9116e3833cabc7e1a94e09
             throw $e;
         }
 
-        // line 1
-        echo "use-ajax";
+        // line 13
+        echo "<div";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "setAttribute", array(0 => "data-vertical-tabs-panes", 1 => true), "method"), "html", null, true));
+        echo ">";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["children"]) ? $context["children"] : null), "html", null, true));
+        echo "</div>
+";
     }
 
     public function getTemplateName()
     {
-        return "{# inline_template_start #}use-ajax";
+        return "core/themes/classy/templates/navigation/vertical-tabs.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
     }
 
     public function getDebugInfo()
     {
-        return array (  43 => 1,);
+        return array (  43 => 13,);
     }
 
     public function getSource()
     {
-        return "{# inline_template_start #}use-ajax";
+        return "{#
+/**
+ * @file
+ * Theme override for vertical tabs.
+ *
+ * Available variables
+ * - attributes: A list of HTML attributes for the wrapper element.
+ * - children: The rendered tabs.
+ *
+ * @see template_preprocess_vertical_tabs()
+ */
+#}
+<div{{ attributes.setAttribute('data-vertical-tabs-panes', TRUE) }}>{{ children }}</div>
+";
     }
 }
