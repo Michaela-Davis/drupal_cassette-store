f742499373ea2c3e0b6ab9b7f1054095b624140e
Guest User <Guest@Epicodus-0F.local>
2017-05-11 16:42:39 -0700

    begin unit test

---
+++ modules/custom/weather/src/TestApi.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * @group weather
+ * Contains \Drupal\weather\src\TestApi.
+ */
+
+ namespace Drupal\weather;
+
+ /**
+  * Provide functions for testing API.
+  *
+  * @package Drupal\weather
+  */
+
+ class TestApi {
+   /**
+    * Return Api Data
+    *
+    * @param $city
+    *
+    * @return string
+   */
+
+  public function api($city) {
+    return "False";
+  }
+
+}
